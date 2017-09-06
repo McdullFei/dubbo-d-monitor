d1dceafde8acf94e2e7141a81588b4b290c7903f
Captain.D.Monkey <renfei198727@gmail.com>
2017-09-06 15:28:47 +0800

    fix bug

--- biz/src/main/java/com/htss/monitor/biz/monitorService/ApplicationClientServiceImpl.java
+++ biz/src/main/java/com/htss/monitor/biz/monitorService/ApplicationClientServiceImpl.java
@@ -1,15 +1,22 @@
 package com.htss.monitor.biz.monitorService;
 
+import com.alibaba.dubbo.common.Constants;
+import com.htss.monitor.bean.MonitorConstants;
+import com.htss.monitor.bean.bizBean.MethodRankBO;
+import com.htss.monitor.biz.bussiness.InvokeBiz;
+import com.htss.monitor.biz.dao.redisManager.InvokeReportManager;
 import com.htss.monitor.biz.support.service.ApplicationService;
 import com.htss.monitor.bean.ResultVO;
 import com.htss.monitor.bean.bizBean.ApplicationBO;
 import com.htss.monitor.bean.bizBean.ServiceBO;
+import com.htss.monitor.common.tools.TimeUtil;
 import com.htss.monitor.service.ApplicationClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -21,6 +28,12 @@ public class ApplicationClientServiceImpl implements ApplicationClientService {
 
   @Autowired
   private ApplicationService applicationService;
+
+  @Autowired
+  private InvokeReportManager invokeReportManager;
+
+  @Autowired
+  private InvokeBiz invokeBiz;
 
   @Override
   public ResultVO getAllAPPAndRelation() {
@@ -82,21 +95,220 @@ public class ApplicationClientServiceImpl implements ApplicationClientService {
 
   @Override
   public ResultVO getSuccessByConsumer(String type, String source) {
-    return null;
+    Map<String, Object> resultMap = new HashMap<>();
+
+    List<String> recentDateList = getRecentDay(type);
+    ApplicationBO applicationBO = applicationService.getApplicationsBOMap().get(source);
+    Set<String> providerSet = applicationBO.getProvidersSet();
+    Set<String> consumerSet = applicationBO.getConsumersSet();
+
+    if (providerSet == null) providerSet = new HashSet<>();
+    if (consumerSet == null) consumerSet = new HashSet<>();
+
+    Map<String, Integer> providerMap = new HashMap<>();
+    Map<String, Integer> consumerMap = new HashMap<>();
+    // 即是消费者 又是提供者
+    Map<String, Map<String, Integer>> providerConsumerMap = new HashMap<>();
+    resultMap.put(Constants.PROVIDER, providerMap);
+    resultMap.put(Constants.CONSUMER, consumerMap);
+    resultMap.put("providerConsumer", providerConsumerMap);
+
+    // 已appStyle 为 consumer 为主 统计
+    for (String date : recentDateList) {
+      Map<String, Map<String, Integer>> appDayMap = invokeReportManager.getAppRelationByAppOnDay(source, date);
+      if (null != appDayMap && !appDayMap.isEmpty()) {
+
+        for (Map.Entry<String, Map<String, Integer>> appDayEntry : appDayMap.entrySet()) {
+          String appType = appDayEntry.getKey();
+          Map<String, Integer> dayMap = appDayEntry.getValue();
+
+          Map<String, Integer> resultTypeMap = (Map<String, Integer>) resultMap.get(appType);
+          for (Map.Entry<String, Integer> dayEntry : dayMap.entrySet()) {
+            String appName = dayEntry.getKey();
+            Integer sum = dayEntry.getValue();
+
+            Integer oldSum = resultTypeMap.get(appName);
+            if (oldSum == null) {
+              oldSum = 0;
+            }
+            sum += oldSum;
+            resultTypeMap.put(appName, sum);
+          }
+        }
+
+      }
+    }
+
+    // 生成二者共有的，并补充无记录数的数据
+    Set<String> consumerKeySet = consumerMap.keySet();
+    Set<String> providerKeySet = providerMap.keySet();
+    Integer sum = 0;
+
+    for (String providerName : providerSet) {
+      if (consumerSet.contains(providerName)) {
+        Integer providerSum = providerMap.get(providerName) == null ? 0 : providerMap.get(providerName);
+//                Integer consumerSum = consumerMap.get(providerName);
+        Map<String, Integer> map = new HashMap<>();
+        map.put(Constants.PROVIDER, providerSum);
+        providerConsumerMap.put(providerName, map);
+        providerMap.remove(providerName);
+      } else {
+        if (!providerKeySet.contains(providerName)) {
+          providerMap.put(providerName, sum);
+        }
+      }
+    }
+
+    for (String consumerName : consumerSet) {
+      if (providerSet.contains(consumerName)) {
+        Integer consumerSum = consumerMap.get(consumerName) == null ? 0 : consumerMap.get(consumerName);
+        providerConsumerMap.get(consumerName).put(Constants.CONSUMER, consumerSum);
+        consumerMap.remove(consumerName);
+      } else {
+        if (!consumerKeySet.contains(consumerName)) {
+          consumerMap.put(consumerName, sum);
+        }
+      }
+    }
+    return ResultVO.wrapSuccessfulResult(resultMap);
   }
 
   @Override
   public ResultVO getSuccessByConsumerOnHour(String type, String source) {
-    return null;
+    //{consumers : time : success/fail}
+    Map<String, Object> resultMap = new HashMap<>();
+
+
+    ApplicationBO applicationBO = applicationService.getApplicationsBOMap().get(source);
+    Set<String> consumerSet = applicationBO.getConsumersSet();
+
+    if (consumerSet == null) consumerSet = new HashSet<>();
+
+    List<String> recentDateList = getRecentDay(type);
+
+    for (String date : recentDateList) {
+      Map<String, Object> map = (Map<String, Object>) invokeReportManager.getConsumerByAppOnHour(source, date);
+      if (null != map && !map.isEmpty()) {
+        for (Map.Entry<String, Object> consumerEntry : map.entrySet()) {
+          String consumerName = consumerEntry.getKey();
+          Map<String, Object> timeMap = (Map<String, Object>) consumerEntry.getValue();
+
+          Map<String, Object> resultTimeMap = (Map<String, Object>) resultMap.get(consumerName);
+          if (resultTimeMap == null) {
+            resultTimeMap = new HashMap<>();
+            resultMap.put(consumerName, resultTimeMap);
+          }
+          for (Map.Entry<String, Object> timeEntry : timeMap.entrySet()) {
+            String time = timeEntry.getKey();
+            Map<String, Integer> sumMap = (Map<String, Integer>) timeEntry.getValue();
+
+            Map<String, Integer> resultSumMap = (Map<String, Integer>) resultTimeMap.get(time);
+            if (resultSumMap == null) {
+              resultSumMap = new HashMap<>();
+              resultTimeMap.put(time, resultSumMap);
+            }
+            Integer resultSuccessNum = resultSumMap.get(MonitorConstants.SUCCESS) == null ? 0 : resultSumMap.get(MonitorConstants.SUCCESS);
+            Integer resultFailNum = resultSumMap.get(MonitorConstants.FAIL) == null ? 0 : resultSumMap.get(MonitorConstants.FAIL);
+
+            resultSuccessNum += sumMap.get(MonitorConstants.SUCCESS);
+            resultFailNum += sumMap.get(MonitorConstants.FAIL);
+            resultSumMap.put(MonitorConstants.SUCCESS, resultSuccessNum);
+            resultSumMap.put(MonitorConstants.FAIL, resultFailNum);
+          }
+        }
+      }
+    }
+
+    //补充为0的数据
+    for (String consumerName : consumerSet) {
+      Map<String, Object> hourSumMap = (Map<String, Object>) resultMap.get(consumerName);
+      if (null == hourSumMap) {
+        hourSumMap = new HashMap<>();
+        resultMap.put(consumerName, hourSumMap);
+      }
+    }
+
+    return ResultVO.wrapSuccessfulResult(resultMap);
   }
 
   @Override
   public ResultVO getSuccessByConsumerOnDay(String type, String source) {
-    return null;
+    //{consumers：day: success/fail}
+    //recentDateList
+    Map<String, Object> finalMap = new HashMap<>();
+    Map<String, Object> resultDataMap = new HashMap<>();
+
+    finalMap.put("dataMap",resultDataMap);
+
+
+
+    ApplicationBO applicationBO = applicationService.getApplicationsBOMap().get(source);
+    Set<String> consumerSet = applicationBO.getConsumersSet();
+
+    if (consumerSet == null) consumerSet = new HashSet<>();
+
+    List<String> recentDateList = getRecentDay(type);
+
+    finalMap.put("dateList",recentDateList);
+    for (String date : recentDateList) {
+      Map<String, Map<String, Integer>> map = invokeReportManager.getConsumerByAppOnDay(source, date);
+      for (Map.Entry<String, Map<String, Integer>> consumerEntry : map.entrySet()) {
+        String consumerName = consumerEntry.getKey();
+        Map<String, Integer> sumMap = consumerEntry.getValue();
+
+        Map<String, Object> resultTimeMap = (Map<String, Object>) resultDataMap.get(consumerName);
+        if (resultTimeMap == null) {
+          resultTimeMap = new HashMap<>();
+          resultDataMap.put(consumerName, resultTimeMap);
+        }
+        resultTimeMap.put(date,sumMap);
+      }
+    }
+
+    //补充为0的数据
+    for (String consumerName : consumerSet) {
+      Map<String, Object> resultTimeMap = (Map<String, Object>) resultDataMap.get(consumerName);
+      if (null == resultTimeMap) {
+        resultTimeMap = new HashMap<>();
+        resultDataMap.put(consumerName, resultTimeMap);
+      }
+    }
+
+    return ResultVO.wrapSuccessfulResult(finalMap);
   }
 
   @Override
   public ResultVO getMethodRanking(String appName) {
-    return null;
+    List<MethodRankBO> list = invokeBiz.getMethodRankByAppName(appName);
+
+    return ResultVO.wrapSuccessfulResult(list);
+  }
+
+  //=======private======
+  private List<String> getRecentDay(String type) {
+    Integer limit = 0;
+    Date date = new Date(System.currentTimeMillis());
+
+    List<String> recentDateList = new ArrayList<>();
+    if (type.equals("Today")) {
+      String nowDate = TimeUtil.getDateString(date);
+      recentDateList.add(nowDate);
+    }  else {
+      switch (type) {
+        case "Seven_DAY":
+          limit = 7;
+          break;
+        case "Fifteen_DAT":
+          limit = 15;
+          break;
+        case "Yesterday":
+          limit = 1;
+          break;
+      }
+      for (Integer amount = -limit; amount < 0; amount++) {
+        recentDateList.add(TimeUtil.getBeforDateByNumber(date, amount));
+      }
+    }
+    return recentDateList;
   }
 }
